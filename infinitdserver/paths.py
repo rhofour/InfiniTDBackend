8ca42b4b9b8d013d24621393e8fc455203eba19b
R. Alex Hofer <me@rofer.me>
2020-09-05 11:31:48 -0400

    Add initial pathfinding code and tests.

---
+++ infinitdserver/paths.py
@@ -0,0 +1,67 @@
+from typing import List, Tuple, Set
+
+from infinitdserver.game_config import CellPos
+from infinitdserver.battleground_state import BattlegroundState
+
+def findShortestPaths(battleground: BattlegroundState, start: CellPos, end: CellPos) -> List[List[Tuple[int, int]]]:
+    """Finds all of the shortest paths from start to end.
+
+    This method infers the size of the battleground from it.
+    """
+
+    rows = len(battleground.towers.towers)
+    cols = len(battleground.towers.towers[0])
+    print(f"Going from {start} to {end}")
+    print(f"{rows} rows and {cols} cols")
+    print(battleground)
+
+    def neighbors(cell: CellPos) -> List[CellPos]:
+        out = []
+        if cell.row > 0:
+            out.append(CellPos(cell.row - 1, cell.col))
+
+        if cell.row < rows - 1:
+            out.append(CellPos(cell.row + 1, cell.col))
+
+        if cell.col > 0:
+            out.append(CellPos(cell.row, cell.col - 1))
+
+        if cell.col < cols - 1:
+            out.append(CellPos(cell.row, cell.col + 1))
+        return out
+
+    # Turn the battleground into an occupancy map.
+    occupiedOrSeen: Set[CellPos] = set()
+    for (row, rowTowers) in enumerate(battleground.towers.towers):
+        for (col, tower) in enumerate(rowTowers):
+            if tower is not None:
+                occupiedOrSeen.add(CellPos(row, col))
+
+    paths = [[start]]
+    i = 0
+    finalPaths = []
+    while paths:
+        i += 1
+        print(f"Step {i}: {paths}")
+        print(f" occupiedOrSeen: {occupiedOrSeen}")
+        newPaths = []
+        newlySeen = set()
+        for partialPath in paths:
+            pathEnd = partialPath[-1]
+            newlySeen.add(pathEnd)
+            if pathEnd == end:
+                finalPaths.append(partialPath)
+                print(" Found path end")
+                continue
+            newNeighbors = [neighbor for neighbor in neighbors(pathEnd) if neighbor not in occupiedOrSeen]
+            print(f" {pathEnd} connects to {newNeighbors}")
+            for neighbor in newNeighbors:
+                newPaths.append(partialPath + [neighbor])
+
+        if finalPaths:
+            break # Skip the remaining steps if we already have the shortest paths
+        paths = newPaths
+        for new in newlySeen:
+            occupiedOrSeen.add(new)
+
+    return finalPaths
---
+++ tests/paths_test.py
@@ -0,0 +1,53 @@
+import unittest
+
+from infinitdserver.game_config import CellPos
+from infinitdserver.battleground_state import BattlegroundState, BgTowersState, BgTowerState
+from infinitdserver.paths import findShortestPaths
+
+def emptyBattleground(rows: int, cols: int):
+    return BattlegroundState(towers = BgTowersState([[None for c in range(cols)] for r in range(rows)]))
+
+class TestPaths(unittest.TestCase):
+    def test_noPath(self):
+        battleground = emptyBattleground(2, 2)
+        battleground.towers.towers[0][1] = BgTowerState(0)
+        battleground.towers.towers[1][0] = BgTowerState(0)
+
+        paths = findShortestPaths(battleground, CellPos(0, 0), CellPos(1, 1))
+
+        self.assertCountEqual(paths, [])
+
+    def test_oneStepPath(self):
+        battleground = emptyBattleground(2, 2)
+
+        paths = findShortestPaths(battleground, CellPos(0, 0), CellPos(0, 1))
+
+        self.assertCountEqual(paths, [[CellPos(0, 0), CellPos(0, 1)]])
+
+    def test_multiStepPath(self):
+        battleground = emptyBattleground(2, 3)
+        print(battleground)
+        battleground.towers.towers[0][1] = BgTowerState(0)
+        print(battleground)
+
+        paths = findShortestPaths(battleground, CellPos(0, 0), CellPos(0, 2))
+
+        self.assertCountEqual(paths, [[CellPos(0, 0), CellPos(1, 0), CellPos(1, 1), CellPos(1, 2), CellPos(0, 2)]])
+
+    def test_multiplePaths(self):
+        battleground = emptyBattleground(3, 3)
+        battleground.towers.towers[1][1] = BgTowerState(0)
+
+        paths = findShortestPaths(battleground, CellPos(0, 0), CellPos(2, 2))
+
+        self.assertCountEqual(paths, [
+            [CellPos(0, 0), CellPos(1, 0), CellPos(2, 0), CellPos(2, 1), CellPos(2, 2)],
+            [CellPos(0, 0), CellPos(0, 1), CellPos(0, 2), CellPos(1, 2), CellPos(2, 2)],
+            ])
+
+    def test_manyPaths(self):
+        battleground = emptyBattleground(3, 3)
+
+        paths = findShortestPaths(battleground, CellPos(0, 0), CellPos(2, 2))
+
+        self.assertEqual(len(paths), 6)
